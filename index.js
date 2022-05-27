8eb7096391f2f461d58470b4a52ad17515c6896a
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-27 09:34:11 +0800

    add route

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('test');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const dbURI = 'mongodb+srv://fangzsx:123123123@cluster0.qtiygwl.mongodb.net/?retryWrites=true&w=majority';
+
+app.get('/', (req, res) => {
+    res.send('homepage');
+})
+
+mongoose.connect(dbURI)
+    .then( () => {
+        const server = app.listen(3000 , () => {
+            console.log(`mongo db connected, listening with port ${server.address().port}`)
+        });
+    })
+
---
+++ routers/products.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+
+//view all products
+router.get('/', (req, res) => {
+
+
+});
+
+//add product
+router.get('/add', (req, res) => {
+
+
+});
